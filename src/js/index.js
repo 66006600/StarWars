d60037b102f6c72edd88e0c1818df4cce74ed465
Beverly M <124820505+66006600@users.noreply.github.com>
2023-05-27 12:46:35 -0400

    Mas cositas2

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,22 @@
 //import react into the bundle
 import React from 'react'
 import {createRoot} from 'react-dom/client'
+import { ReactDOM } from 'react';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
-//import your own components
-import Layout from './layout.js'
+serviceWorker.unregister(); 
 
-//
-const root = createRoot(document.querySelector("#app"))
 
-//render your react application
-root.render(<Layout/>)
+
+
 
